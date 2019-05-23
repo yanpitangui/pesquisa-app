406ab6fc7bf8e59e58c236f3c56cfc5964015775
Yan Carlos Cordeiro Pitangui <yan.pitangui@cscservicos.com.br>
2019-05-23 17:24:06 -0300

    criação repo

---
+++ app/components/HelloWorld.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+/* the main page for the index route of this app */
+const HelloWorld = function() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+
+      <p>This is a starter <a href="http://glitch.com">Glitch</a> app for React! It uses 
+        only a few dependencies to get you started on working with React:</p>
+    </div>
+  );
+}
+
+module.exports = HelloWorld;
